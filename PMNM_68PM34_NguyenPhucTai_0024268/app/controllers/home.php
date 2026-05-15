4a7957da0028f17de4d2e616beafb53456beb6a8
TaiPhuc0302 <taivuahaitac2005@gmail.com>
2026-05-15 15:24:52 +0700

    Khởi tạo dự án

--- PMNM_68PM34_NguyenPhucTai_0024268/app/controllers/home.php
+++
@@ -1,6 +0,0 @@
-<?php
-class home {
-    function index() {
-        echo "Đây là trang chủ";
-    }
-}
--- PMNM_68PM34_NguyenPhucTai_0024268/app/controllers/sinhvien.php
+++
@@ -1,8 +0,0 @@
-<?php
-class sinhvien {
-    function index() {
-        echo "Đây là danh sách sinh viên";
-    }
-
-    
-}
