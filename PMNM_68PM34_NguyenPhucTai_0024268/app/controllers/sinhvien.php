532943894b9f7d9964abde251a35ce08f86914c3
TaiPhuc0302 <taivuahaitac2005@gmail.com>
2026-05-26 10:08:07 +0700

    feat(core): add Controller + ConnectDB
    feat(model): add SinhvienModel::getAll().
    
    feat: Hiển thị danh sách sinh viên.

--- PMNM_68PM34_NguyenPhucTai_0024268/app/controllers/sinhvien.php
+++ PMNM_68PM34_NguyenPhucTai_0024268/app/controllers/sinhvien.php
@@ -1,8 +1,12 @@
 <?php
-class sinhvien {
+require_once '../app/core/Controller.php';
+class sinhvien extends Controller {
     function index() {
+        $sinhvienModel = $this->model('sinhvienModel');
+        $sinhvien = $sinhvienModel -> getAllSinhvien();
         //trả về view 
-        require_once '../app/views/sinhvien/index.php';
+        //require_once '../app/views/sinhvien/index.php';
+        $this -> view('sinhvien/index', ['sinhvien' => $sinhvien]);
     }
 
     function create() {
